e56dfc658c25f788ffb69530ba1ca23e9a9acb1d
Viktor Szakats <commit@vsz.me>
2025-10-04 05:20:59 +0200

    try fixing missing openssl 1.1.0 on appveyor 2

--- appveyor.sh
+++ appveyor.sh
@@ -35,10 +35,10 @@ esac
 
 if [ "${APPVEYOR_BUILD_WORKER_IMAGE}" = 'Visual Studio 2022' ]; then
   openssl_root_win="C:/OpenSSL-v35${openssl_suffix}"
-elif [ "${APPVEYOR_BUILD_WORKER_IMAGE}" = 'Visual Studio 2019' ]; then
-  openssl_root_win="C:/OpenSSL-v111${openssl_suffix}"
 elif [ "${APPVEYOR_BUILD_WORKER_IMAGE}" = 'Visual Studio 2013' ]; then
   openssl_root_win="C:/OpenSSL${openssl_suffix}"
+else
+  openssl_root_win="C:/OpenSSL-v111${openssl_suffix}"
 fi
 openssl_root="$(cygpath "${openssl_root_win}")"
 
